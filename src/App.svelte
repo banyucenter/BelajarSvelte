c82bdf9a0655ad8edf60b8651c0779e8b07f5714
Ipung Purwono <ipungofficial@gmail.com>
2020-08-17 19:02:35 +0700

    reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,76 @@
 <script>
-  import Other from "./Other/Other.svelte";
+  // import Other from "./Other/Other.svelte";
   import NavigationBar from "./NavigationBar/NavigationBar.svelte";
-  import { Col, Container, Row } from "sveltestrap";
+  import {
+    Col,
+    Container,
+    Row,
+    Button,
+    Card,
+    CardTitle,
+    CardSubtitle,
+    CardBody,
+    CardImg,
+    Alert,
+  } from "sveltestrap";
 
-  let name = "Jack Ma";
-  let skill = {
-    namaSkill: ["Svelte", "PHP", "ReactJs", "VueJS"],
-    ratingSkill: 80,
-  };
+  let keranjang = 0;
+  let harga = 10000000;
+  let diskon = 0.1;
+  //deklaratif
+  $: sebelumdiskon = keranjang * harga;
+  $: setelahdiskon = keranjang * harga - keranjang * harga * diskon;
 
-  let imageURL = "image/logosvelte.png";
+  function handleClick() {
+    keranjang += 1;
+  }
 </script>
 
 <style>
-  .nama {
+  /* .nama {
     color: red;
-  }
+  } */
 </style>
 
 <NavigationBar />
+<br />
 <Container>
-  <main>
+  <h2>Pembelian Produk</h2>
+  <hr />
+  <Row>
 
-    <h2>
-      Welcome,
-      <span class="nama">{name}</span>
-    </h2>
-    <hr />
-    <p>
-      Skill :
-      <b>{skill.namaSkill[0]} dan {skill.namaSkill[3]}</b>
-    </p>
-    <p>Rating: {skill.ratingSkill}</p>
-    <img src={imageURL} alt="Logo Svelte" width="300" />
+    <Col xs="4">
+      <Card>
+        <br />
+        <CardImg
+          src="image/mbp2019.jpg"
+          alt="Card image cap"
+          width="300"
+          class="mb-3" />
+        <CardBody>
+          <CardTitle class="mb-3">
+            <h2>Macbook Pro 2020</h2>
+          </CardTitle>
+          <Alert color="danger">Selamat mendapatkan Diskon 10%</Alert>
+          <CardSubtitle>Best Laptop in 2020</CardSubtitle>
+          <br />
+          <CardSubtitle class="mb-3">
+            <b>Harga Rp. {harga}</b>
+          </CardSubtitle>
+          <Button color="primary" on:click={handleClick}>Add to Cart</Button>
+        </CardBody>
+      </Card>
 
-    <!--Other komponen-->
-    <Other />
-  </main>
+    </Col>
+    <Col xs="3">
+      <Card body color="light" class="mb-3">Jumlah pesanan: {keranjang}</Card>
+      <Card body color="light" class="mb-3">
+        Harga Sebelum Diskon: {sebelumdiskon}
+      </Card>
+      <Card body color="danger" inverse>
+        Harga Setelah Diskon : {setelahdiskon}
+      </Card>
+
+    </Col>
+  </Row>
 </Container>
