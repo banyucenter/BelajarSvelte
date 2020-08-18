43e6bab59f8ec53088f64175138f3166ba95e3ee
Ipung Purwono <ipungofficial@gmail.com>
2020-08-18 10:14:12 +0700

    if else view

--- src/App.svelte
+++ src/App.svelte
@@ -2,54 +2,34 @@
   import { Col, Container, Row, Button, ListGroup } from "sveltestrap";
   import NavigationBar from "./NavigationBar/NavigationBar.svelte";
   import ListItem from "./ListItem/ListItem.svelte";
-  // import Card from "./Card/Card.svelte";
-  // import Other from "./Other/Other.svelte";
+  import Welcome from "./Welcome/Welcome.svelte";
 
-  // let keranjang = 0
-  // let harga = 1000000
-
-  // let diskon = {
-  //   diskon3 : 0.1,
-  //   diskon10: 0.3
-  // }
-
-  // $: hargatotal = keranjang * harga
-
-  // $: if(keranjang >= 10){
-  //   hargatotal = keranjang * harga - keranjang * harga * diskon.diskon10
-  // }else if(keranjang >=3) {
-  //   hargatotal = keranjang * harga - keranjang * harga * diskon.diskon3
-  // }else {
-  //   hargatotal
-  // }
-
-  // function tambahKeranjang() {
-  //   keranjang += 1
-  // }
-
-  const mbp = {
-    produk: "Macbook Pro 2020",
-    summary: "Laptop Kece badai guys",
+  const newlaptop = {
+    produk: "Asus Gaming",
+    desc: "Laptop Gaming Keren",
   };
+
+  let user = {
+    username : "Ipung Dev",
+    role: 1
+  }
+
 </script>
 
 <NavigationBar />
 <br />
 <Container>
-
+  <Welcome role={user.role} user={user.username}/>
   <Row>
-    <!-- <Col><Button on:click={tambahKeranjang}>Add to Cart</Button></Col>
-    <Col> Jumlah Keranjang : {keranjang} 
-      <br>Harga total : Rp. {hargatotal}</Col> -->
-    <Col md="6">
+    <Col>
       <ListGroup>
-        <ListItem produk={'Macbook 2019'} summary={'laptop kece badai'} />
-        <ListItem {...mbp} />
-        <ListItem produk={'Lenovo 2020'} />
+        <ListItem produk={'Macbook Pro 2020'} desc={'Ini adalah laptop'} />
+        <ListItem produk={'Dell Mountain 2020'} />
+        <ListItem {...newlaptop} />
+        <ListItem />
         <ListItem />
       </ListGroup>
     </Col>
-
   </Row>
 
 </Container>
--- src/ListItem/ListItem.svelte
+++ src/ListItem/ListItem.svelte
@@ -1,13 +1,17 @@
 <script>
-    import { ListGroupItem, Row, Col } from 'sveltestrap';
-    export let produk ="Tidak ada merk";
-    export let summary ="Informasi laptop"
-
+  import { ListGroupItem, Row, Col } from "sveltestrap";
+  export let produk = "Tidak ada merk";
+  export let desc ="Tidak ada deskripsi";
 </script>
 
-<ListGroupItem tag="a" href="https://svelte.dev" action>
-    <Row>
-        <Col xs="2"><img src="image/mbp2019.jpg" alt="gambar" width="60"/></Col>
-        <Col xs="8"><h5>{produk}</h5><p>{summary}</p></Col>
-    </Row>
+<ListGroupItem>
+  <Row>
+    <Col>
+      <img src="image/mbp2019.jpg" alt="gambar ini" width="100" />
+    </Col>
+    <Col>
+      <h5>{produk}</h5>
+      <p>{desc}</p>
+    </Col>
+  </Row>
 </ListGroupItem>
